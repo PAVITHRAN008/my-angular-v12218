52e8ae06a9272d014c53afe7a0dfeb9644bcfbe9
Pavithran Lallu <pavithranlallu@Pavithran-MacBook-Air.local>
2025-09-05 18:43:14 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "https://userservice-jyz8.onrender.com/api",
+  //apiUrl: "https://userservice-jyz8.onrender.com/api",
+  apiUrl: "http://localhost:5200/api",
 };
